4aef6adbf755e8eb02342dad82e47d109cfcd3b9
sg3957 <sg3957@nyu.edu>
2018-11-01 17:22:39 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,16 +1,27 @@
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
-  
+  before_action :set_student, only: [:show, :activate]
+
   def index
     @students = Student.all
   end
 
   def show
+    # @student from set_student
+  end
+
+  def activate
+    # @student from set_student
+    student_status = @student.active #false or true based on current status
+    @student.active = !student_status #active attribute in the table is going to be the opposite of what it originally was
+    @student.save
+    redirect_to @student
   end
 
   private
 
     def set_student
+      #params -> {id: 1}
       @student = Student.find(params[:id])
+      # @student
     end
 end
